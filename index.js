c8bae19fc4cbd25a6c6656bdca64500b00b263bf
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-11-12 19:34:34 +0530

    web server using nodeJs explore static and dynamin index file

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const lib = require("./lib.js");
-const express = require("express");
-// const fs = require("fs");
-console.log("hello");
+const fs = require("fs");
+const http = require("http");
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+const server = http.createServer((req, res) => {
+  console.log(req.url);
 
-const server = express();
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const product = products.find((p) => p.id == +id);
+    res.setHeader("Content-Type", "text/html");
+    let modifiedIndex = index
+      .replace("**title**", product.title)
+      .replace("**url**", product.thumbnail)
+      .replace("**price**", product.price)
+      .replace("**rating**", product.rating);
+    res.end(modifiedIndex);
+    return;
+  }
+
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
+
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(data);
+      break;
+    default:
+      res.writeHead(404);
+      res.end();
+  }
+  console.log("server started");
+  //   res.setHeader("dummy", "demo");
+});
 server.listen(8080);
-// const t1 = performance.now();
-// const text = fs.readFileSync("./demo.txt", "utf-8");
-// fs.readFile("./demo.txt", "utf-", (err, text) => {
-//   console.log(text, "text");
-// // });
-// console.log(text, "text");
-// console.log(lib.sum(10, 89), lib.diff(10, 89));
-// const t2 = performance.now();
-// console.log(t2 - t1);
-// const a = 5;
