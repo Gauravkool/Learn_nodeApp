973c1f7c38d96a8ad0f6e08934ec4129d52b5788
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-11-13 20:00:30 +0530

    Model View Controller (MCV) and file structure

--- index.js
+++ index.js
@@ -1,74 +1,16 @@
-const fs = require("fs");
-const index = fs.readFileSync("index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = data.products;
-
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const server = express();
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
 
-// bodyParser
+//bodyParser
 server.use(express.json());
-server.use(morgan("default"));
-server.use(express.static("public"));
-
-// API - Endpoint - Route
-// Products
-// API Root, base URL, example- google.com/api/v2
-
-// Create POST /products
-server.post("/products", (req, res) => {
-  console.log(req.body);
-  products.push(req.body);
-  res.status(201).json(req.body);
-});
-
-// Read GET /products
-server.get("/products", (req, res) => {
-  res.json(products);
-});
-
-// Read GET /products/:id
-server.get("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const product = products.find((p) => p.id === id);
-  res.json(product);
-});
-
-// Update PUT /products/:id
-server.put("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json();
-});
-
-// Update PATCH /products/:id
-server.patch("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json();
-});
-
-// Delete /products:/id
-server.delete("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
-});
-
-server.get("/demo", (req, res) => {
-  // res.send("<h1>hello</h1>");
-  // res.sendFile("D:/nodeApp/index.html");
-  // res.json(products)
-  // res.sendStatus(404)
-  // res.status(201).send("<h1>hello</h1>")
-});
+server.use(morgan('default'));
+server.use(express.static('public'));
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
 
 server.listen(8080, () => {
-  console.log("server started");
+  console.log('server started');
 });
