9ccab38e6a7025bc80a3520453d4cb24e6ecddf7
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-11-15 18:47:57 +0530

    connect to frontend react app

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const server = express();
+const path = require("path");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 console.log("env", process.env.DB_PASSWORD);
@@ -17,11 +18,15 @@ async function main() {
 }
 
 //bodyParser
+server.use(cors());
 server.use(express.json());
 server.use(morgan("default"));
-server.use(express.static(process.env.PUBLIC_DIR));
+server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use("/products", productRouter.router);
 server.use("/users", userRouter.router);
+server.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 server.listen(process.env.PORT, () => {
   console.log("server started");
--- model/product.js
+++ model/product.js
@@ -14,6 +14,7 @@ const productSchema = new Schema({
     type: Number,
     min: [0, "wrong min rating"],
     max: [5, "wrong max rating"],
+    default: 0,
   },
   brand: { type: String, required: true },
   category: { type: String, required: true },
