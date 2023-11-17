c35064aeee420ee6eda2a20cdde6bf6262442486
Gaurav Kumar <g.ksinghania77@gmail.com>
2023-11-17 22:35:15 +0530

    fix vercel json file static hosting

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 // const index = fs.readFileSync('index.html', 'utf-8');
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const path = require("path");
+const data = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../data.json", "utf-8"))
+);
 const users = data.users;
-
 
 exports.createUser = (req, res) => {
   console.log(req.body);
